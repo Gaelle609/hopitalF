137b3c395dc80c91f7ed74abf81a43038b318e5d
Alphred TATONG <alphredtatong@gmail.com>
2025-10-23 17:00:57 +0100

    Mes modifications locales

--- src/pages/personnel/index.vue
+++ src/pages/personnel/index.vue
@@ -430,7 +430,8 @@ export default {
             formData,
             {
               headers: {
-                "Content-Type": "application/json",
+                "Content-Type": "multipart/form-data",
+                "Accept": "application/json",
                 Authorization: `Bearer ${token}`
               }
             }
